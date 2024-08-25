114a2c1b1642b2ee6f3e011b90eee84f1c78c3d9
Andres Villarreal <villarrealtovar@gmail.com>
2024-08-25 11:58:47 -0500

    Akka Essentials: Changing Behavior I

---
+++ rockthejvm/akka-essentials/src/main/scala/lesson01Actors/L03ChangingActorBehavior.scala
@@ -0,0 +1,87 @@
+package lesson01Actors
+
+import org.apache.pekko.actor.Actor
+import org.apache.pekko.actor.ActorRef
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.actor.Props
+import lesson01Actors.L03ChangingActorBehavior.Mom.MomStart
+
+object L03ChangingActorBehavior extends App {
+
+  object FussyKid {
+    case object KidAccept
+    case object KidReject
+
+    val HAPPY = "happy"
+    val SAD = "sad"
+  }
+
+  class FussyKid extends Actor {
+    import FussyKid._
+    import Mom._
+
+    var state = HAPPY
+
+    def receive: Receive = {
+      case Food(VEGETABLE) => state = SAD
+      case Food(CHOCOLATE) => state = HAPPY
+      case Ask(_) if state == HAPPY =>
+        sender() ! KidAccept
+      case Ask(_) if state == SAD =>
+        sender() ! KidReject
+    }
+  }
+
+  object Mom {
+    case class MomStart(kidRef: ActorRef)
+    case class Food(food: String)
+    case class Ask(message: String)
+
+    val VEGETABLE = "veggies"
+    val CHOCOLATE = "chocolate"
+  }
+
+  class Mom extends Actor {
+    import Mom._
+    import FussyKid._
+
+    def receive: Receive = {
+      case MomStart(kidRef) =>
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Ask("do you want to play?")
+      case KidAccept => println("Yay, my kid is happy")
+      case KidReject => println("My kid is sad, but as he's healthy!")
+    }
+  }
+
+  val system = ActorSystem("changingActorBehaviorDemo")
+
+  val fussyKid = system.actorOf(Props[FussyKid])
+  val mom = system.actorOf(Props[Mom])
+
+  mom ! MomStart(fussyKid)
+
+  class StatelessFussyKid extends Actor {
+    import FussyKid._
+    import Mom._
+
+    def receive: Receive = happyReceive
+
+    def happyReceive: Receive = {
+      case Food(VEGETABLE) => context.become(sadReceive)
+      case Food(CHOCOLATE) =>
+      case Ask(_)          => sender() ! KidAccept
+    }
+
+    def sadReceive: Receive = {
+      case Food(VEGETABLE) =>
+      case Food(CHOCOLATE) => context.become(happyReceive)
+      case Ask(_)          => sender() ! KidReject
+    }
+  }
+
+  val statelessFussyKid = system.actorOf(Props[StatelessFussyKid])
+
+  mom ! MomStart(statelessFussyKid)
+
+}
