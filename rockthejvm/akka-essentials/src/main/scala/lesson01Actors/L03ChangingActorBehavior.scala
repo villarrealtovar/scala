f6bb76f73b47e7713634dc1b48ab3a5a2192e321
Andres Villarreal <villarrealtovar@gmail.com>
2024-08-25 22:00:00 -0500

    Akka Essentials: Exercise Changing Behavior: Counter immutable state and Vote Aggregator with mutable state

--- rockthejvm/akka-essentials/src/main/scala/lesson01Actors/L03ChangingActorBehavior.scala
+++ rockthejvm/akka-essentials/src/main/scala/lesson01Actors/L03ChangingActorBehavior.scala
@@ -87,4 +87,119 @@ object L03ChangingActorBehavior extends App {
 
   mom ! MomStart(statelessFussyKid)
 
+  /** Exercise 1.
+    *
+    * Recreate Counter Actor with context become and NO MUTABLE State
+    */
+  object Counter {
+    case object Increment
+    case object Decrement
+    case object Print
+  }
+  class Counter extends Actor {
+    import Counter._
+
+    /* def receive: Receive = {
+      case Increment(value) => count += value
+      case Decrement(value) => count -= value
+      case Print            => println(s"[counter]: my current count is $count")
+    } */
+
+    def receive: Receive = countReceive(0)
+
+    def countReceive(currentCount: Int): Receive = {
+      case Increment =>
+        println(s"[countReceive($currentCount)]: incrementing")
+        context.become(countReceive(currentCount + 1))
+      case Decrement =>
+        println(s"[countReceive($currentCount)]: decrementing")
+        context.become(countReceive(currentCount - 1))
+      case Print =>
+        println(
+          s"[countReceive($currentCount)]: my current count is $currentCount"
+        )
+    }
+
+  }
+
+  val counter = system.actorOf(Props[Counter], "counter")
+
+  import Counter._
+
+  (1 to 5).foreach(_ => counter ! Increment)
+  (1 to 3).foreach(_ => counter ! Decrement)
+  counter ! Print
+
+  /** Exercise 2.
+    *
+    * A simplified voting system
+    */
+
+  object Citizen {
+    case class Vote(candidate: String)
+    case object VoteStatusRequest
+    case class VoteStatusReply(candidate: Option[String])
+  }
+
+  class Citizen extends Actor {
+    import Citizen._
+
+    var candidate: Option[String] = None
+
+    override def receive: Receive = {
+      case Vote(c)           => candidate = Some(c)
+      case VoteStatusRequest => sender() ! VoteStatusReply(candidate)
+    }
+  }
+
+  object VoteAggregator {
+    case class AggregateVotes(citizens: Set[ActorRef])
+  }
+
+  class VoteAggregator extends Actor {
+    import VoteAggregator._
+    import Citizen._
+
+    var stillWaiting: Set[ActorRef] = Set.empty
+    var currentStats: Map[String, Int] = Map.empty
+
+    override def receive: Receive = {
+      case AggregateVotes(citizens) =>
+        stillWaiting = citizens
+        citizens.foreach(citizenRef => citizenRef ! VoteStatusRequest)
+      case VoteStatusReply(None) => sender() ! VoteStatusRequest
+      case VoteStatusReply(Some(candidate)) =>
+        val newStillWaiting = stillWaiting - sender()
+        val currentVotesOfCandidate = currentStats.getOrElse(candidate, 0)
+        currentStats =
+          currentStats + (candidate -> (currentVotesOfCandidate + 1))
+        if (newStillWaiting.isEmpty)
+          println(s"[VoteAggregator]: poll stats: $currentStats")
+        else stillWaiting = newStillWaiting
+    }
+  }
+
+  val alice = system.actorOf(Props[Citizen])
+  val bob = system.actorOf(Props[Citizen])
+  val charlie = system.actorOf(Props[Citizen])
+  val daniel = system.actorOf(Props[Citizen])
+
+  import Citizen._
+
+  alice ! Vote("Martin")
+  bob ! Vote("Jonas")
+  charlie ! Vote("Roland")
+  daniel ! Vote("Roland")
+
+  val voteAggregator = system.actorOf(Props[VoteAggregator])
+
+  import VoteAggregator._
+
+  voteAggregator ! AggregateVotes(Set(alice, bob, charlie, daniel))
+
+  /** Print the status of the votes
+    *
+    * Martin -> 1 Jonas -> 1 Roland -> 2
+    */
+
 }
